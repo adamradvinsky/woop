0dff9779aec9e686049a6dc978c84817536ac025
Adam Radvinsky <radvinskyadam@gmail.com>
2026-09-09 13:10:14 -0700

    recieved bytes from strap; must put strap into purple (pairing mode)

--- obj/Debug/net10.0-windows10.0.19041.0/woop.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/woop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("woop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1af94e1e9b0b3c3c30ad8bb2f5187d6e6b7a616")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d727d89627812a8383ef348787e7d77917711fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("woop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("woop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/StrapClient.cs
+++ src/StrapClient.cs
@@ -43,6 +43,14 @@ namespace strap
         uuid: fd4b0001-cce1-4033-93ce-002d5875f58a
         */
 
+        /*
+        AA-01-74-00-01-00-3F-B1-24-00-91-01-02-01-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-0B-B4-F7-F8
+
+        AA-01-74-00-01-00-3F-B1-24-01-91-01-01-01-B5-01-00-00-00-AC-BC-A1-6A-F5-48-00-00-35-42-30-30-32-33-39-31-31-33-00-31-62-30-36-36-35-35-63-30-31-61-30-37-65-31-65-39-32-34-62-62-63-66-31-64-37-66-61-65-63-37-36-36-64-61-35-63-37-38-38-63-61-34-30-62-65-37-35-61-39-34-34-66-38-0D-00-00-00-00-00-00-00-52-00-00-00-32-23-03-00-00-00-00-0A-03-20-01-01-00-00-00-00-B7-A9-46-3B
+        
+        08-42-71-59-51-58-66-45-48-74-43-6D-77-31-30-54-4F-38-59-61-48-4F-63-4E-51-54-5A-34-78-4C-6D-5A-61-31-00-A7-02-02-03-01-0A-68-6D-61-76-65-72-69-63-6B-09-69-35-30-2E-33-35-2E-33-2E-30-06-68-57-47-35-30-5F-72-35-32-01-1A-6A-A1-BC-A7-04-A5-01-19-10-01-02-1A-00-05-87-B2-03-1A-00-05-88-89-04-01-05-00-F2-07 
+        */
+
         private GattDeviceServicesResult services;
         private GattCharacteristic CMD_TO_STRAP;
         private GattCharacteristic CMD_FROM_STRAP;
@@ -113,12 +121,12 @@ namespace strap
             Console.WriteLine("sending hello yo ");
 
              byte[] rawBytes = new byte[] 
-        { 
+            { 
             0xAA, 0x01, 0x08, 0x00, 0x00, 0x01, 0xE6, 0x71, 
             0x23, 0x01, 0x91, 0x01, 0x36, 0x3E, 0x5C, 0x8D 
-        };
+            };
 
-        // 2. Convert the byte array into an IBuffer
+            // 2. Convert the byte array into an IBuffer
             IBuffer data = rawBytes.AsBuffer();
 
             GattCommunicationStatus result = await CMD_TO_STRAP.WriteValueAsync(data, GattWriteOption.WriteWithResponse);
@@ -158,7 +166,7 @@ namespace strap
             byte[] data = new byte[reader.UnconsumedBufferLength];
             reader.ReadBytes(data);
 
-            Console.WriteLine(data);
+            Console.WriteLine(BitConverter.ToString(data));
 
             // byte flags = data[0];
             // bool is16Bit = (flags & 0x1) == 1;
