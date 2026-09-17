e0640053edcda66ed86e497dc835d762f59d2062
Adam Radvinsky <radvinskyadam@gmail.com>
2026-09-17 15:34:25 -0700

    sent proper message and fixing stuff up

--- obj/Debug/net10.0-windows10.0.19041.0/woop.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/woop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("woop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d2730c634cc615f2078cdea6c33b1f8eda156f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dcc64656ccca64d90adb53fa8447be4f8d442f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("woop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("woop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
